937b29e685573c0e176215420f160f56eb01fb5d
dnsxmrs <ericemarial@gmail.com>
2025-01-05 22:09:58 +0800

    running vite

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
+
+// import { defineConfig } from 'vite';
+
+// // Dynamically import the laravel-vite-plugin as an ES Module
+// const laravel = await import('laravel-vite-plugin');
+
+// export default defineConfig({
+//   plugins: [
+//     laravel.default(), // or laravel() depending on how it's exported
+//   ],
+// });
