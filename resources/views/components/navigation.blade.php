61a9e200afa5ce7ff9819b4326f8257a5ce330d8
dnsxmrs <ericemarial@gmail.com>
2025-01-30 12:57:09 +0800

    Remove commented-out Admin button in navigation component

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -14,10 +14,10 @@
             <!-- Search Bar, Basket Icon, and Buttons -->
             <div class="flex items-center space-x-2">
                 <!-- Admin Button -->
-                <button
+                {{-- <button
                     class="flex items-center justify-center w-40 h-10 px-6 py-3 text-lg text-white bg-black rounded-full hover:bg-brown-600">
                     <img src="assets/Default User.png" alt="User" class="w-5 h-5 mr-2">Admin
-                </button>
+                </button> --}}
             </div>
         </div>
     </header>
